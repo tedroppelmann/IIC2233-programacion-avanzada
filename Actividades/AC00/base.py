f7a0b03be914b9394ba41bb2727a9deb5e77202b
Tomás Droppelmann Pidal <tomasdroppelmannpidal@MacBook-Pro-de-Tomas.local>
2020-03-16 12:22:56 -0300

    listo

--- Actividades/AC00/base.py
+++ Actividades/AC00/base.py
@@ -1,10 +1,15 @@
 # Seccion para importar liberías
 
 class Cuadrado(object):
-	def __init__(self, arg):
+	def __init__(self, p1, p2, p3, p4):
+		self.p1 = p1
+		self.p2 = p2
+		self.p3 = p3
+		self.p4 = p4
 		# Completar método y borrar pass
 		pass
 	# Agregar métodos
+	
 
 class Triangulo(object):
 	def __init__(self, arg):
@@ -14,4 +19,5 @@ class Triangulo(object):
 		
 		
 if __name__ == '__main__':
+	input()
 	# Crear instancias aquí
