4c00fb0d9b77ac7ac4ffff1fe0a3675747a3a6ab
Tomás Droppelmann Pidal <tomasdroppelmannpidal@MacBook-Pro-de-Tomas.local>
2020-03-17 00:42:19 -0300

    menú principal

--- Tareas/T00/menu.py
+++ Tareas/T00/menu.py
@@ -1,23 +1,23 @@
 
 import sys
-
 import usuarios
 
 print("Bienvenido a DCCahuin!!")
 
 def menu():
     while True:
+        print("MENÚ DE INICIO")
         print("Seleccione una opción:")
         print("[1] Iniciar sesión")
         print("[2] Registrar usuario")
         print("[0] Salir")
         respuesta = input("Indique su opción (0, 1 o 2):")
         if respuesta == str(1):
-            nombre = input("Ingrese el nombre de usuario:")
-            usuarios.ingresar(nombre)
+            usuario = input("Ingrese el nombre de usuario:")
+            usuarios.ingresar(usuario)
         elif respuesta == str(2):
-            nombre = input("Indique el nuevo nombre de usuario:")
-            usuarios.agregar(nombre)
+            usuario = input("Indique el nuevo nombre de usuario:")
+            usuarios.agregar(usuario)
         elif respuesta == str(0):
             sys.exit()
         else:
--- Tareas/T00/usuarios.py
+++ Tareas/T00/usuarios.py
@@ -1,50 +1,46 @@
 
 import os
-
 import prograpost
+import seguidores
 
 ruta_usuarios = os.path.join("usuarios.csv")
+ruta_segudiores = os.path.join("seguidores.csv")
 
-file = open(ruta_usuarios, "a")
-file.write("\n")
-file.close()
 
-def agregar(nombre):
+def agregar(usuario):
     file = open(ruta_usuarios)
     for line in file:
-        if line.rstrip() == nombre:
+        if line.rstrip() == usuario:
             print("El nombre de usuario ya existe.")
             file.close()
             return
     file2 = open(ruta_usuarios, "a")
-    file2.write(str(nombre) + "\n")
+    file2.write(str(usuario) + "\n")
     file2.close()
+    file3 = open(ruta_segudiores, "a")
+    file3.write(str(usuario)+ "\n")
+    file3.close()
     print("Usuario agregado exitosamente!")
 
 
-def ingresar(nombre):
+def ingresar(usuario):
     file = open(ruta_usuarios)
     lines = file.readlines()
     file.close()
-    if str(nombre + "\n") in lines:
+    if str(usuario + "\n") in lines:
         while True:
-            print("Bienvenido " + str(nombre) + ". Elija un menú:")
+            print("Bienvenido " + str(usuario) + ". Elija un menú:")
             print("[1] Menú de prograpost")
             print("[2] Menú de seguidores")
-            print("[0] Regresar al menú anterior")
+            print("[0] Regresar al menú de inicio")
             respuesta = input("Indique su opción (0, 1 o 2):")
             if respuesta == str(1):
-                prograpost.menu()
-                return False
+                prograpost.menu(usuario)
             elif respuesta == str(2):
-                print("SEGUIDORES")
-                return False
+                seguidores.menu(usuario)
             elif respuesta == str(0):
                 return False
             else:
                 print("Input incorrecto. Intente nuevamente.")
     else:
         print("El nombre de usuario no existe. Intente nuevamente.")
-
-
-
