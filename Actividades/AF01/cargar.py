04ec4dd781c33a4bff6cecd1d602b9c0a2508b46
Tomás Droppelmann Pidal <tomasdroppelmannpidal@MacBook-Pro-de-Tomas.local>
2020-03-19 15:58:07 -0300

    upgrade

--- Actividades/AF01/cargar.py
+++ Actividades/AF01/cargar.py
@@ -5,21 +5,30 @@ def cargar_animes(path):
     # Abrimos el archivo de animes
     with open(path, 'r', encoding="utf-8") as file:
         # Leemos las lineas
+        animes = dict()
+        registro = namedtuple("nombres_anime",["nombre", "rating", "estudio", "genero"])
         for line in file.readlines():
             # Las separamos por coma
             anime = line.strip().split(",")
             # Separamos los generos por slash
             anime[3] = anime[3].split("/")
+            anime[3] = set(anime[3])
+            anime[3] = list(anime[3])
+            genero = " ".join(anime[3])
+            c = registro(anime[0], anime[1], anime[2], genero)
+            animes[c.nombre] = (c.rating, c.estudio, c.genero)
+    return animes
 
-    return
 
 
 def cargar_consultas(path):
     # Abrimos el archivo de animes
     with open(path, 'r', encoding="utf-8") as file:
         # Leemos las lineas
+        cola = deque()
         for line in file.readlines():
             # Los separamos por coma
             consulta = line.strip().split(";")
-
-    return
+            argumentos = consulta[1].split(";")
+            cola.append((consulta[0], argumentos))
+    return cola
--- Actividades/AF01/consultas.py
+++ Actividades/AF01/consultas.py
@@ -2,12 +2,20 @@ from collections import defaultdict
 
 
 def cantidad_animes_genero(animes):
-    pass
+    generos = dict()
+    for i in animes:
+        if i[2] not in generos:
+            generos[i[2]] = 1
+        elif i[2] in generos:
+            generos[i[2]] += 1
+    return generos
 
 
 def generos_distintos(anime, animes):
+    print(animes)
     pass
 
 
 def promedio_rating_genero(animes):
+    print(animes)
     pass
